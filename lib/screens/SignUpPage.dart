42e519c2ef6e7d896936980d25d80d4e92eb60e4
sharmjikabeta <grandraghav@outlook.com>
2020-06-03 23:48:43 +0530

    Debug karo bhaiya

--- lib/screens/SignUpPage.dart
+++ lib/screens/SignUpPage.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:groceryhome/constants/constants.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:groceryhome/screens/FirstScreen.dart';
 import 'package:groceryhome/widgets/custom_text_field.dart';
-
 import '../constants/constants.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+//final _firestore = Firestore.instance;
+//FirebaseUser loggedInUser;
 class SignUpPage extends StatefulWidget {
   static String id = 'SignUpPage';
 
@@ -13,6 +16,7 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  //final _auth = FirebaseAuth.instance;
   String _name = '';
   String _email = '';
   String _number = '';
@@ -125,13 +129,31 @@ class _SignUpPageState extends State<SignUpPage> {
               ),
               SizedBox(height: MediaQuery.of(context).size.height / 18),
               GestureDetector(
-                onTap: () {
+                onTap: () async {
                   if (_confirmPassword != _password && _password != '')
-                    Scaffold.of(context).showSnackBar(
+                    {Scaffold.of(context).showSnackBar(
                       SnackBar(
                         content: Text('Passwords dont Match'),
                       ),
                     );
+//                    }else{
+//                    try{
+//                      final newUser = await _auth.createUserWithEmailAndPassword(email: _email, password: _password);
+//                      if(newUser != null){
+//                        Navigator.pushNamed(context,FirstScreen.id);
+//                      }
+//                      _firestore.collection('users').add({
+//                        'email':newUser.user.email,
+//                        'phone':_number,
+//                        'password': _password,
+//                        'name':_name
+//                      });
+//                    }catch(e){
+//                      print(e);
+//                    }
+
+                  }
+
                 }, //TODO Implemending New User Creation
                 child: Container(
                   height: 48,
